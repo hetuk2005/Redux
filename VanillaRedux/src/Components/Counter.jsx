72cefa3be15284c9aecbe9778e1097dd6de06d19
hetuk2005 <patelhetuk2005@gmail.com>
2025-12-23 17:03:06 +0530

    Code

---
+++ VanillaRedux/src/Components/Counter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Counter = () => {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <>
+      <h1>Counter {count}</h1>
+      <button onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+    </>
+  );
+};
