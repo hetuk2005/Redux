9031e006113c9bc8a49d8d3e63f54fc0a89a8299
hetuk2005 <patelhetuk2005@gmail.com>
2026-01-19 16:59:11 +0530

    Code

--- local-redux1/src/Components/Counter.jsx
+++ local-redux1/src/Components/Counter.jsx
@@ -35,20 +35,57 @@ export const Counter = () => {
 
   return (
     <>
-      <h1>Counter {value}</h1>
-      <button onClick={handleInc}>+</button>
-      <button onClick={handleDec}>-</button>
-      <br />
-      <br />
-      <input
-        type="number"
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="Enter Value"
-      />
-      <br />
-      <br />
-      <button onClick={handleIncByValue}>++</button>
-      <button onClick={handleDecByValue}>--</button>
+      <div
+        style={{
+          width: "100%",
+          margin: "auto",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>Counter {value}</h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "15px",
+          }}
+        >
+          <button onClick={handleInc}>+</button>
+          <button onClick={handleDec}>-</button>
+        </div>
+        <br />
+        <br />
+        <input
+          style={{
+            borderRadius: "15px",
+            padding: "7px 11px",
+            cursor: "pointer",
+            fontWeight: "700",
+            letterSpacing: "1px",
+            fontSize: "15px",
+          }}
+          type="number"
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter Value"
+        />
+        <br />
+        <br />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "15px",
+          }}
+        >
+          <button onClick={handleIncByValue}>++</button>
+          <button onClick={handleDecByValue}>--</button>
+        </div>
+      </div>
     </>
   );
 };
--- local-redux1/src/Pages/Home.jsx
+++ local-redux1/src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <h1 style={{ textAlign: "center" }}>Home</h1>
     </>
   );
 };
