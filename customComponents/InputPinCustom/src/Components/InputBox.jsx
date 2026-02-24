1ec7b4bed84cc4cbd8548a600bd17a3380ba463b
hetuk2005 <patelhetuk2005@gmail.com>
2026-02-24 22:10:53 +0530

    Code

--- customComponents/InputPinCustom/src/Components/InputBox.jsx
+++ customComponents/InputPinCustom/src/Components/InputBox.jsx
@@ -12,7 +12,7 @@ export const InputBox = ({ perBox, setMainVal, length, style }) => {
     vals[i] = v;
     setValues(vals);
 
-    if (vals && i < vals.length - 1) {
+    if (i < vals.length - 1) {
       elements.current[i + 1].focus();
       setMainVal(vals.join(""));
     }
