897056f61fbe9ad77eeb4a1735a104e606f16169
hetuk2005 <patelhetuk2005@gmail.com>
2026-03-09 22:12:38 +0530

    Code

--- musicApp/src/Components/FilterMusicRecords.jsx
+++ musicApp/src/Components/FilterMusicRecords.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 export const FilterMusicRecords = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [filterVal, setFilterVal] = useState([]);
+
+  const handleFilter = (e) => {
+    const option = e.target.name;
+    console.log("Option: ", option);
+
+    const newArr = [...filterVal];
+
+    if (filterVal.includes(option)) {
+      newArr.splice(filterVal.indexOf(option), 1);
+    } else {
+      newArr.push(option);
+    }
+
+    setFilterVal(newArr);
+    // console.log("Filter Value: ", filterVal);
+  };
+
+  // console.log("Filter Value: ", filterVal);
+
   return (
     <>
       <h1>Filter</h1>
